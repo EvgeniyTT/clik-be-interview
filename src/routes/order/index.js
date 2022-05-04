5736f7c32fa48e0862164a2cd02685b94801f15a
Yevhen Tataryn <yevhen.tataryn@coxautoinc.com>
2022-05-04 12:19:55 +0200

    code review

--- src/routes/order/index.js
+++ src/routes/order/index.js
@@ -1,32 +1,50 @@
 import express from 'express';
 
 import { OrderService } from '../../modules/order/order.service';
+import { CourierService } from '../../modules/courier/courier.service';
 
 const router = express.Router();
 
-router.get('/getById/:id', async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
     try {
         const orderId = req.params.id;
         const order = await OrderService.getById(orderId);
-        return res.json({ order });
+        return order 
+            ? res.json({ order })
+            : res.status(404).send(`No orders found by id: ${orderId}`);
     } catch (err) {
         return next(err);
     }
 });
 
-router.post('/courier/assign/:id', async (req, res, next) => {
+router.post('/:id/courier/assign', async (req, res, next) => {
     try {
         const orderId = req.params.id;
-        const courierId = await OrderService.assignNearestCourier(orderId);
+        const order = await OrderService.getById(orderId);
+        if(order?.status !== ORDER_STATUS_ENUM.READY_TO_PICKUP) {
+            throw new Error (`order ${orderId} is not ready to be picked up, order status is ${order?.status}`);
+        }
+
+        const courier = await CourierService.getCouriers({
+            location: order.location,
+            rating: COURIER_RATING_ENUM.GOOD,
+            status: COURIER_STATUS_ENUM.FREE,
+            limit: 1,
+        });
+        if (!courier) {
+            throw new Error (`no available couriers within the order ${orderId} location ${order.location}`);
+        }
+
+        await OrderService.assignCourier(courier);
         
-        return res.json({ status: Boolean(courierId), courierId });
+        return res.json({ status: !!courier, courier });
     } catch (err) {
         return next(err);
     }
 });
 
 // route with some integration logic
-router.post('/integration/check/:id', async (req, res, next) => {
+router.get('/:id/integration/check', async (req, res, next) => {
     try {
         const orderId = req.params.id;
         const statusesArr = await OrderService.checkIntegrationStatus(orderId);
--- test/modules/order/order.service.test.js
+++ test/modules/order/order.service.test.js
@@ -1,5 +1,6 @@
 import { OrderService } from '../../../src/modules/order/order.service';
 
+// TODO: would be nice to write real tests
 describe('#unit OrderService', () => {
     describe('#unit getById', () => {
         test('case', async () => {
